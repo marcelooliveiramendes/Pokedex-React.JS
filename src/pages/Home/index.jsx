1a0171d0099cce604ff927a447a39b145342e1d6
Marcelo Oliveira Mendes <marcelooliveiraah09@gmail.com>
2022-07-05 06:36:59 -0300

    Conectando com API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { Wrapper } from '../../styles/Home';
+import NavBar from '../../components/NavBar';
 
 function Home() {
     const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
@@ -14,21 +15,18 @@ function Home() {
         
         setUrl(data.next)
 
-
         const getPokemonsInfo = async (result) => {
-        result.forEach(async (pokemon) => {
-            const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-                .then((response)=>{return response.data})
-                .catch((err)=>{return console.log("Falha ao requisição do pokemon! \n\n\n" + err)})
+            result.forEach(async (pokemon) => {
+                const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                    .then((response)=>{return response.data})
+                    .catch((err)=>{return console.log("Falha ao requisição do pokemon! \n\n\n" + err)})
 
-            setPokemons(currentList => [...currentList, data])
-        })
-
-            
+                setPokemons(currentList => [...currentList, data])
+            })
         }
 
-
         getPokemonsInfo(data.results)
+        setPokemons([])
         console.log(await pokemons)
         
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -40,7 +38,7 @@ function Home() {
 
     return (
         <Wrapper>
-            
+            <NavBar />
         </Wrapper>
     )
 }
