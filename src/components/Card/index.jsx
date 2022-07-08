d72c89c3b5542ec075f779f6260233da45c6e732
Marcelo Oliveira Mendes <marcelooliveiraah09@gmail.com>
2022-07-07 23:53:54 -0300

    Começando a estilizar o card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,86 @@
+
 import React from 'react'
-import { CardBox } from '../../styles/Card'
+import { CardBox, PokeInfos, PokeTypes, Title, TypePoke } from '../../styles/Card'
 
 
 function Card({props}) {
-    console.log(props)
 
+    let types = props.types
+    let bgColor = '';
+
+    switch (props.types[0].type.name){
+        case 'normal': 
+            bgColor = "#c4c3c3"
+            break;
+        case 'fire': 
+            bgColor = "#ffa860"
+            break; 
+        case 'water': 
+            bgColor = "#37a7f2"
+            break;   
+        case 'grass': 
+            bgColor = "#8ec28c"
+            break;   
+        case 'flying': 
+            bgColor = "#9bb5e5"
+            break;   
+        case 'fighting': 
+            bgColor = "#d64260"
+            break;   
+        case 'poison': 
+            bgColor = "#bd61d1"
+            break;   
+        case 'electric': 
+            bgColor = "#f4d23c"
+            break;   
+        case 'ground': 
+            bgColor = "#dc8253"
+            break;   
+        case 'rock': 
+            bgColor = "#d0c390"
+            break;   
+        case 'psychic': 
+            bgColor = "#f9797c"
+            break;   
+        case 'ice': 
+            bgColor = "#76cfc2"
+            break;
+        case 'bug': 
+            bgColor = "#70B77E"
+            break;   
+        case 'ghost': 
+            bgColor = "#5c6cb7"
+            break;   
+        case 'steel': 
+            bgColor = "#56a5a8"
+            break;   
+        case 'dragon': 
+            bgColor = "#067abe"
+            break;   
+        case 'dark': 
+            bgColor = "##5a5465"
+            break;
+        case 'fairy': 
+            bgColor = "#ed93e2"
+            break;
+
+        default:
+            console.log("Not Found");
+    }
+
+
+    console.log(types);
     return (
-        <CardBox>
-
-        </CardBox>
+        <CardBox backgroundColor={bgColor}>
+            <PokeInfos>
+                <Title>{props.name}</Title>
+                <PokeTypes>
+                    {types.map((type, index)=>(
+                        <TypePoke key={index}>{type.type.name}</TypePoke>
+                    ))}
+                </PokeTypes>
+            </PokeInfos>
+        </CardBox>  
     )
 }
 
--- src/styles/Card.js
+++ src/styles/Card.js
@@ -4,10 +4,28 @@ export const CardBox = styled.div`
     width: 320px;
     height: 150px;
     display: flex;
+    margin:10px;
+    background-color: ${(props) => props.backgroundColor};
 `
 export const PokeInfos = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    padding: 0px 20px;
+`
+export const Title = styled.h3`
+    font-size: 22px;
+    
+`
+export const PokeTypes = styled.ul`
+    display: flex;
+    list-style: none;
+
+
+`
+export const TypePoke = styled.li`
+    &:nth-child(2){
+        margin-left: 20px;
+    }
 `
